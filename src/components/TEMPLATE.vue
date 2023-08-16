08c6d9a30f2d9a78133fdbbc37101645fbb44c05
NotNatural21 <notnatural21@gmail.com>
2023-08-16 12:29:32 +1200

    init

---
+++ src/components/TEMPLATE.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'TEMPLATE',
+	props: {
+
+	},
+	methods: {
+
+	},
+})
+
+</script>
+
+<template>
+	<div>
+
+	</div>
+</template>
+
+<style scoped>
+
+</style>
